77f9b30616ee1edd8fd50de2a15232abf209468f
sergio <sergio.quiroz2012@gmail.com>
2021-03-21 16:31:54 -0700

    now need to populate website

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
 
-
-
-
 //click button will add class
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
@@ -11,7 +8,7 @@ navToggle.addEventListener('click', () => {
 
 //nav links 
 navLinks.forEach(link => {
-    navLinks.addEventListener('click', () => {
+    link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
-    });
-});
+    })
+})
